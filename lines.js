f719d69d2cc74b265c09be37e06695dd51efa5e6
Julien Dau <julien.dau@student.htw-berlin.de>
2016-11-22 00:51:55 +0100

    clean up lines.js

--- lines.js
+++ lines.js
@@ -1,29 +1,7 @@
 var lines = {
-	/*
-	"U1": {
-		color:"#00FF00",
-		stations: ["WA","S","Gr","Kbo","Pr","Ho","Mo","Go","Kus","Nu","Wt","Kfu","U"],
-		offsets: [null,null,null,null,null,null,null,null,null,null,null,null,null]
-	},
-	"U3": {
-		color:"#22CCAA",
-		stations: ["Nu","Wt","Au", "Snu","Hz","Fpo","HEI","Rd","Bt","Po","Dd","T","Os","Oth","K"],
-		offsets: [{x:0,y:1},{x:0,y:1},null,null,null,null,null,null,null,null,null,null,null,null,null]
-	},
-	"U4": {
-		color:"#FFFF00",
-		stations: ["Nu", "V", "Bpo", "RS", "Ipo"],
-		offsets: [null,null,null,null,null]
-	},
-	"U9": {
-		color:"#FF6600",
-		stations: ["Olo","Np","Lpu","Am","WF","Bi","Tm","Ha","Zu","Kfu","Snu","Gt","Beo","Bd","Fw","Wsg","Slu","Rzu"],
-		offsets: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
-	},
-	*/
 	"S41": {
 		color:"#CC3344",
 		stations: [9001201, 9009104, 9007102, 9110001, 9110002, 9110003, 9110004, 9110012, 9120001, 9120003, 9190001, 9077106, 9078201, 9079221, 9068201, 9058101, 9054104, 9054105, 9044202, 9045102, 9044101, 9040101, 9024102, 9024106, 9026207, 9020201, 9020202, 9001201],
-		offsets: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+		offsets: [],
 	}
 };
