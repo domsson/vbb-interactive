5c8b6245f692f2079b947a962953d0427596877c
Julien Dau <julien.dau@student.htw-berlin.de>
2016-11-22 00:11:33 +0100

    S and S+U stations with geo locations and international station number as id

---
+++ stations_vbb.js
@@ -0,0 +1,794 @@
+var stations = {
+	9003102: {
+		type: "S",
+		name: "Bellevue",
+		geo: {y:52.5199510, x:13.3470980},
+		pos: {x:0, y:0}
+	},
+	9003103: {
+		type: "S",
+		name: "Tiergarten",
+		geo: {y:52.5139630, x:13.3362350},
+		pos: {x:0, y:0}
+	},
+	9007104: {
+		type: "S",
+		name: "Nordbahnhof",
+		geo: {y:52.5316730, x:13.3887940},
+		pos: {x:0, y:0}
+	},
+	9008101: {
+		type: "Humboldthain",
+		name: "S",
+		geo: {y:52.5447420, x:13.3787260},
+		pos: {x:0, y:0}
+	},
+	9012101: {
+		type: "S",
+		name: "Anhalter Bahnhof",
+		geo: {y:52.5045380, x:13.3820770},
+		pos: {x:0, y:0}
+	},
+	9020202: {
+		type: "S",
+		name: "Beusselstr.",
+		geo: {y:52.5343140, x:13.3287030},
+		pos: {x:0, y:0}
+	},
+	9024101: {
+		type: "S",
+		name: "Charlottenburg Bhf",
+		geo: {y:52.5050520, x:13.3052120},
+		pos: {x:0, y:0}
+	},
+	9024102: {
+		type: "S",
+		name: "Westkreuz",
+		geo: {y:52.5011520, x:13.2830360},
+		pos: {x:0, y:0}
+	},
+	9024106: {
+		type: "S",
+		name: "Messe Nord/ICC",
+		geo: {y:52.5064540, x:13.2830430},
+		pos: {x:0, y:0}
+	},
+	9024203: {
+		type: "S",
+		name: "Savignyplatz",
+		geo: {y:52.5052230, x:13.3190030},
+		pos: {x:0, y:0}
+	},
+	9025321: {
+		type: "S",
+		name: "Olympiastadion",
+		geo: {y:52.5111350, x:13.2411110},
+		pos: {x:0, y:0}
+	},
+	9025423: {
+		type: "S",
+		name: "Messe Süd",
+		geo: {y:52.4987710, x:13.2704500},
+		pos: {x:0, y:0}
+	},
+	9025424: {
+		type: "S",
+		name: "Pichelsberg",
+		geo: {y:52.5102640, x:13.2271960},
+		pos: {x:0, y:0}
+	},
+	9026105: {
+		type: "S",
+		name: "Heerstr.",
+		geo: {y:52.5082730, x:13.2585140},
+		pos: {x:0, y:0}
+	},
+	9026207: {
+		type: "S",
+		name: "Westend",
+		geo: {y:52.5186100, x:13.2842370},
+		pos: {x:0, y:0}
+	},
+	9029101: {
+		type: "S",
+		name: "Spandau Bhf",
+		geo: {y:52.5347980, x:13.1974770},
+		pos: {x:0, y:0}
+	},
+	9030202: {
+		type: "S",
+		name: "Stresow",
+		geo: {y:52.5325050, x:13.2091300},
+		pos: {x:0, y:0}
+	},
+	9040101: {
+		type: "S",
+		name: "Halensee",
+		geo: {y:52.4966980, x:13.2901510},
+		pos: {x:0, y:0}
+	},
+	9044101: {
+		type: "S",
+		name: "Hohenzollerndamm",
+		geo: {y:52.4883140, x:13.3007120},
+		pos: {x:0, y:0}
+	},
+	9048101: {
+		type: "S",
+		name: "Grunewald",
+		geo: {y:52.4886800, x:13.2619440},
+		pos: {x:0, y:0}
+	},
+	9049201: {
+		type: "S",
+		name: "Zehlendorf",
+		geo: {y:52.4312090, x:13.2592270},
+		pos: {x:0, y:0}
+	},
+	9049202: {
+		type: "S",
+		name: "Sundgauer Str.",
+		geo: {y:52.4361870, x:13.2735010},
+		pos: {x:0, y:0}
+	},
+	9050301: {
+		type: "S",
+		name: "Mexikoplatz",
+		geo: {y:52.4371660, x:13.2320600},
+		pos: {x:0, y:0}
+	},
+	9050355: {
+		type: "S",
+		name: "Schlachtensee",
+		geo: {y:52.4398060, x:13.2139860},
+		pos: {x:0, y:0}
+	},
+	9052201: {
+		type: "S",
+		name: "Nikolassee",
+		geo: {y:52.4312100, x:13.1932980},
+		pos: {x:0, y:0}
+	},
+	9053301: {
+		type: "S",
+		name: "Wannsee Bhf",
+		geo: {y:52.4214570, x:13.1790990},
+		pos: {x:0, y:0}
+	},
+	9054104: {
+		type: "S",
+		name: "Schöneberg",
+		geo: {y:52.4798110, x:13.3528480},
+		pos: {x:0, y:0}
+	},
+	9057104: {
+		type: "S",
+		name: "Julius-Leber-Brücke",
+		geo: {y:52.4862630, x:13.3607750},
+		pos: {x:0, y:0}
+	},
+	9058101: {
+		type: "S",
+		name: "Südkreuz Bhf",
+		geo: {y:52.4754680, x:13.3655790},
+		pos: {x:0, y:0}
+	},
+	9058102: {
+		type: "S",
+		name: "Priesterweg",
+		geo: {y:52.4592390, x:13.3560280},
+		pos: {x:0, y:0}
+	},
+	9060101: {
+		type: "S",
+		name: "Friedenau",
+		geo: {y:52.4700020, x:13.3405980},
+		pos: {x:0, y:0}
+	},
+	9063101: {
+		type: "S",
+		name: "Feuerbachstr.",
+		geo: {y:52.4635780, x:13.3324120},
+		pos: {x:0, y:0}
+	},
+	9063452: {
+		type: "S",
+		name: "Südende",
+		geo: {y:52.4488990, x:13.3540770},
+		pos: {x:0, y:0}
+	},
+	9064201: {
+		type: "S",
+		name: "Lichterfelde Süd",
+		geo: {y:52.4099560, x:13.3086620},
+		pos: {x:0, y:0}
+	},
+	9064256: {
+		type: "S",
+		name: "Osdorfer Str.",
+		geo: {y:52.4183730, x:13.3138650},
+		pos: {x:0, y:0}
+	},
+	9064301: {
+		type: "S",
+		name: "Lichterfelde Ost Bhf",
+		geo: {y:52.4290980, x:13.3280780},
+		pos: {x:0, y:0}
+	},
+	9066101: {
+		type: "S",
+		name: "Lichterfelde West",
+		geo: {y:52.4435120, x:13.2941410},
+		pos: {x:0, y:0}
+	},
+	9066102: {
+		type: "S",
+		name: "Botanischer Garten",
+		geo: {y:52.4475520, x:13.3060410},
+		pos: {x:0, y:0}
+	},
+	9067221: {
+		type: "S",
+		name: "Lankwitz",
+		geo: {y:52.4390340, x:13.3427600},
+		pos: {x:0, y:0}
+	},
+	9068301: {
+		type: "S",
+		name: "Attilastr.",
+		geo: {y:52.4472980, x:13.3611770},
+		pos: {x:0, y:0}
+	},
+	9072101: {
+		type: "S",
+		name: "Buckower Chaussee",
+		geo: {y:52.4109190, x:13.3826890},
+		pos: {x:0, y:0}
+	},
+	9073101: {
+		type: "S",
+		name: "Marienfelde",
+		geo: {y:52.4238160, x:13.3750220},
+		pos: {x:0, y:0}
+	},
+	9074201: {
+		type: "S",
+		name: "Lichtenrade",
+		geo: {y:52.3877060, x:13.3963580},
+		pos: {x:0, y:0}
+	},
+	9074202: {
+		type: "S",
+		name: "Schichauweg",
+		geo: {y:52.3985630, x:13.3894570},
+		pos: {x:0, y:0}
+	},
+	9077106: {
+		type: "S",
+		name: "Sonnenallee",
+		geo: {y:52.4738220, x:13.4559980},
+		pos: {x:0, y:0}
+	},
+	9077155: {
+		type: "S",
+		name: "Köllnische Heide",
+		geo: {y:52.4691680, x:13.4675290},
+		pos: {x:0, y:0}
+	},
+	9084101: {
+		type: "S",
+		name: "Wilhelmsruh",
+		geo: {y:52.5815880, x:13.3632840},
+		pos: {x:0, y:0}
+	},
+	9085105: {
+		type: "S",
+		name: "Alt-Reinickendorf",
+		geo: {y:52.5779690, x:13.3497470},
+		pos: {x:0, y:0}
+	},
+	9085201: {
+		type: "S",
+		name: "Schönholz",
+		geo: {y:52.5719010, x:13.3802420},
+		pos: {x:0, y:0}
+	},
+	9089303: {
+		type: "S",
+		name: "Tegel",
+		geo: {y:52.5888050, x:13.2886950},
+		pos: {x:0, y:0}
+	},
+	9091203: {
+		type: "S",
+		name: "Heiligensee",
+		geo: {y:52.6251480, x:13.2286670},
+		pos: {x:0, y:0}
+	},
+	9091205: {
+		type: "S",
+		name: "Schulzendorf",
+		geo: {y:52.6135130, x:13.2456300},
+		pos: {x:0, y:0}
+	},
+	9092201: {
+		type: "S",
+		name: "Frohnau",
+		geo: {y:52.6329580, x:13.2895640},
+		pos: {x:0, y:0}
+	},
+	9093201: {
+		type: "S",
+		name: "Hermsdorf",
+		geo: {y:52.6181650, x:13.3068040},
+		pos: {x:0, y:0}
+	},
+	9094101: {
+		type: "S",
+		name: "Waidmannslust",
+		geo: {y:52.6068140, x:13.3205490},
+		pos: {x:0, y:0}
+	},
+	9096405: {
+		type: "S",
+		name: "Eichborndamm",
+		geo: {y:52.5775450, x:13.3155830},
+		pos: {x:0, y:0}
+	},
+	9100002: {
+		type: "S",
+		name: "Hackescher Markt",
+		geo: {y:52.5226070, x:13.4023580},
+		pos: {x:0, y:0}
+	},
+	9100007: {
+		type: "S",
+		name: "Oranienburger Str.",
+		geo: {y:52.5251610, x:13.3930680},
+		pos: {x:0, y:0}
+	},
+	9110002: {
+		type: "S",
+		name: "Prenzlauer Allee",
+		geo: {y:52.5448020, x:13.4274190},
+		pos: {x:0, y:0}
+	},
+	9110003: {
+		type: "S",
+		name: "Greifswalder Str.",
+		geo: {y:52.5407240, x:13.4383560},
+		pos: {x:0, y:0}
+	},
+	9110004: {
+		type: "S",
+		name: "Landsberger Allee",
+		geo: {y:52.5287720, x:13.4559440},
+		pos: {x:0, y:0}
+	},
+	9110011: {
+		type: "S",
+		name: "Bornholmer Str.",
+		geo: {y:52.5547540, x:13.3978400},
+		pos: {x:0, y:0}
+	},
+	9110012: {
+		type: "S",
+		name: "Storkower Str.",
+		geo: {y:52.5241950, x:13.4648840},
+		pos: {x:0, y:0}
+	},
+	9120003: {
+		type: "S",
+		name: "Ostkreuz Bhf",
+		geo: {y:52.5028510, x:13.4690930},
+		pos: {x:0, y:0}
+	},
+	9120005: {
+		type: "S",
+		name: "Ostbahnhof",
+		geo: {y:52.5100010, x:13.4357470},
+		pos: {x:0, y:0}
+	},
+	9120011: {
+		type: "S",
+		name: "Warschauer Str.",
+		geo: {y:52.5077480, x:13.4509920},
+		pos: {x:0, y:0}
+	},
+	9130001: {
+		type: "S",
+		name: "Pankow-Heinersdorf",
+		geo: {y:52.5776030, x:13.4290900},
+		pos: {x:0, y:0}
+	},
+	9130003: {
+		type: "S",
+		name: "Wollankstr.",
+		geo: {y:52.5649740, x:13.3931500},
+		pos: {x:0, y:0}
+	},
+	9135001: {
+		type: "S",
+		name: "Buch",
+		geo: {y:52.6366140, x:13.4924520},
+		pos: {x:0, y:0}
+	},
+	9142001: {
+		type: "S",
+		name: "Blankenburg",
+		geo: {y:52.5910690, x:13.4429720},
+		pos: {x:0, y:0}
+	},
+	9143001: {
+		type: "S",
+		name: "Karow Bhf",
+		geo: {y:52.6157550, x:13.4700810},
+		pos: {x:0, y:0}
+	},
+	9151001: {
+		type: "S",
+		name: "Wartenberg",
+		geo: {y:52.5732660, x:13.5042380},
+		pos: {x:0, y:0}
+	},
+	9152001: {
+		type: "S",
+		name: "Hohenschönhausen Bhf",
+		geo: {y:52.5665490, x:13.5136980},
+		pos: {x:0, y:0}
+	},
+	9152002: {
+		type: "S",
+		name: "Gehrenseestr.",
+		geo: {y:52.5557360, x:13.5256530},
+		pos: {x:0, y:0}
+	},
+	9160001: {
+		type: "S",
+		name: "Rummelsburg",
+		geo: {y:52.5012130, x:13.4786990},
+		pos: {x:0, y:0}
+	},
+	9160002: {
+		type: "S",
+		name: "Betriebsbahnhof Rummelsburg",
+		geo: {y:52.4939570, x:13.4975090},
+		pos: {x:0, y:0}
+	},
+	9160003: {
+		type: "S",
+		name: "Nöldnerplatz ",
+		geo: {y:52.5034440, x:13.4842970},
+		pos: {x:0, y:0}
+	},
+	9162001: {
+		type: "S",
+		name: "Karlshorst Bhf",
+		geo: {y:52.4810380, x:13.5259810},
+		pos: {x:0, y:0}
+	},
+	9170001: {
+		type: "S",
+		name: "Marzahn",
+		geo: {y:52.5432720, x:13.5415240},
+		pos: {x:0, y:0}
+	},
+	9170002: {
+		type: "S",
+		name: "Raoul-Wallenberg-Str.",
+		geo: {y:52.5507490, x:13.5483920},
+		pos: {x:0, y:0}
+	},
+	9170003: {
+		type: "S",
+		name: "Mehrower Allee",
+		geo: {y:52.5578160, x:13.5543920},
+		pos: {x:0, y:0}
+	},
+	9170004: {
+		type: "S",
+		name: "Ahrensfelde Bhf",
+		geo: {y:52.5715640, x:13.5661180},
+		pos: {x:0, y:0}
+	},
+	9170005: {
+		type: "S",
+		name: "Poelchaustr.",
+		geo: {y:52.5347510, x:13.5360080},
+		pos: {x:0, y:0}
+	},
+	9171001: {
+		type: "S",
+		name: "Biesdorf",
+		geo: {y:52.5130590, x:13.5550350},
+		pos: {x:0, y:0}
+	},
+	9171002: {
+		type: "S",
+		name: "Friedrichsfelde Ost",
+		geo: {y:52.5137470, x:13.5200430},
+		pos: {x:0, y:0}
+	},
+	9171003: {
+		type: "S",
+		name: "Springpfuhl",
+		geo: {y:52.5258060, x:13.5369040},
+		pos: {x:0, y:0}
+	},
+	9175002: {
+		type: "S",
+		name: "Kaulsdorf",
+		geo: {y:52.5121220, x:13.5890300},
+		pos: {x:0, y:0}
+	},
+	9176001: {
+		type: "S",
+		name: "Mahlsdorf",
+		geo: {y:52.5121180, x:13.6112300},
+		pos: {x:0, y:0}
+	},
+	9180001: {
+		type: "S",
+		name: "Köpenick",
+		geo: {y:52.4586920, x:13.5795430},
+		pos: {x:0, y:0}
+	},
+	9180002: {
+		type: "S",
+		name: "Wuhlheide",
+		geo: {y:52.4690580, x:13.5535270},
+		pos: {x:0, y:0}
+	},
+	9180003: {
+		type: "S",
+		name: "Spindlersfeld",
+		geo: {y:52.4459380, x:13.5627010},
+		pos: {x:0, y:0}
+	},
+	9182001: {
+		type: "S",
+		name: "Hirschgarten",
+		geo: {y:52.4579030, x:13.6014800},
+		pos: {x:0, y:0}
+	},
+	9182002: {
+		type: "S",
+		name: "Friedrichshagen",
+		geo: {y:52.4570900, x:13.6254620},
+		pos: {x:0, y:0}
+	},
+	9183001: {
+		type: "S",
+		name: "Rahnsdorf",
+		geo: {y:52.4513710, x:13.6916550},
+		pos: {x:0, y:0}
+	},
+	9183002: {
+		type: "S",
+		name: "Wilhelmshagen",
+		geo: {y:52.4385710, x:13.7210040},
+		pos: {x:0, y:0}
+	},
+	9186001: {
+		type: "S",
+		name: "Grünau",
+		geo: {y:52.4127140, x:13.5740180},
+		pos: {x:0, y:0}
+	},
+	9190001: {
+		type: "S",
+		name: "Treptower Park",
+		geo: {y:52.4930220, x:13.4608870},
+		pos: {x:0, y:0}
+	},
+	9191001: {
+		type: "S",
+		name: "Baumschulenweg",
+		geo: {y:52.4675810, x:13.4895050},
+		pos: {x:0, y:0}
+	},
+	9191002: {
+		type: "S",
+		name: "Plänterwald",
+		geo: {y:52.4787220, x:13.4740290},
+		pos: {x:0, y:0}
+	},
+	9192001: {
+		type: "S",
+		name: "Schöneweide Bhf",
+		geo: {y:52.4546110, x:13.5101490},
+		pos: {x:0, y:0}
+	},
+	9192002: {
+		type: "S",
+		name: "Oberspree",
+		geo: {y:52.4522690, x:13.5384610},
+		pos: {x:0, y:0}
+	},
+	9193001: {
+		type: "S",
+		name: "Betriebsbahnhof Schöneweide",
+		geo: {y:52.4466950, x:13.5240280},
+		pos: {x:0, y:0}
+	},
+	9193002: {
+		type: "S",
+		name: "Adlershof",
+		geo: {y:52.4351020, x:13.5405530},
+		pos: {x:0, y:0}
+	},
+	9195510: {
+		type: "S",
+		name: "Altglienicke",
+		geo: {y:52.4077910, x:13.5596020},
+		pos: {x:0, y:0}
+	},
+	9196001: {
+		type: "S",
+		name: "Grünbergallee",
+		geo: {y:52.3995390, x:13.5432220},
+		pos: {x:0, y:0}
+	},
+	9001201: {
+		type: "S+U",
+		name: "Westhafen",
+		geo: {y:52.5361830, x:13.3438370},
+		pos: {x:0, y:0}
+	},
+	9007102: {
+		type: "S+U",
+		name: "Gesundbrunnen Bhf ",
+		geo: {y:52.5486370, x:13.3883720},
+		pos: {x:0, y:0}
+	},
+	9009104: {
+		type: "S+U",
+		name: "Wedding",
+		geo: {y:52.5427320, x:13.3660600},
+		pos: {x:0, y:0}
+	},
+	9020201: {
+		type: "S+U",
+		name: "Jungfernheide Bhf",
+		geo: {y:52.5302750, x:13.2990640},
+		pos: {x:0, y:0}
+	},
+	9023201: {
+		type: "S+U",
+		name: "Zoologischer Garten Bhf",
+		geo: {y:52.5069210, x:13.3327070},
+		pos: {x:0, y:0}
+	},
+	9029302: {
+		type: "S+U",
+		name: "Rathaus Spandau",
+		geo: {y:52.5357980, x:13.1998910},
+		pos: {x:0, y:0}
+	},
+	9044202: {
+		type: "S+U",
+		name: "Bundesplatz",
+		geo: {y:52.4773660, x:13.3291490},
+		pos: {x:0, y:0}
+	},
+	9045102: {
+		type: "S+U",
+		name: "Heidelberger Platz",
+		geo: {y:52.4794460, x:13.3118430},
+		pos: {x:0, y:0}
+	},
+	9054105: {
+		type: "S+U",
+		name: "Innsbrucker Platz",
+		geo: {y:52.4781000, x:13.3428750},
+		pos: {x:0, y:0}
+	},
+	9057103: {
+		type: "S+U",
+		name: "Yorckstr.",
+		geo: {y:52.4927660, x:13.3704290},
+		pos: {x:0, y:0}
+	},
+	9062202: {
+		type: "S+U",
+		name: "Rathaus Steglitz",
+		geo: {y:52.4550660, x:13.3221520},
+		pos: {x:0, y:0}
+	},
+	9068201: {
+		type: "S+U",
+		name: "Tempelhof",
+		geo: {y:52.4706940, x:13.3857540},
+		pos: {x:0, y:0}
+	},
+	9078201: {
+		type: "S+U",
+		name: "Neukölln",
+		geo: {y:52.4692820, x:13.4436920},
+		pos: {x:0, y:0}
+	},
+	9079221: {
+		type: "S+U",
+		name: "Hermannstr.",
+		geo: {y:52.4671810, x:13.4317040},
+		pos: {x:0, y:0}
+	},
+	9096101: {
+		type: "S+U",
+		name: "Wittenau",
+		geo: {y:52.5963920, x:13.3354810},
+		pos: {x:0, y:0}
+	},
+	9096458: {
+		type: "S+U",
+		name: "Karl-Bonhoeffer-Nervenklinik",
+		geo: {y:52.5781690, x:13.3329210},
+		pos: {x:0, y:0}
+	},
+	9100001: {
+		type: "S+U",
+		name: "Friedrichstr. Bhf",
+		geo: {y:52.5202700, x:13.3871530},
+		pos: {x:0, y:0}
+	},
+	9100003: {
+		type: "S+U",
+		name: "Alexanderplatz Bhf",
+		geo: {y:52.5215120, x:13.4112670},
+		pos: {x:0, y:0}
+	},
+	9100004: {
+		type: "S+U",
+		name: "Jannowitzbrücke",
+		geo: {y:52.5155000, x:13.4180270},
+		pos: {x:0, y:0}
+	},
+	9100020: {
+		type: "S+U",
+		name: "Potsdamer Platz Bhf",
+		geo: {y:52.5093400, x:13.3764540},
+		pos: {x:0, y:0}
+	},
+	9100025: {
+		type: "S+U",
+		name: "Brandenburger Tor",
+		geo: {y:52.5165110, x:13.3819370},
+		pos: {x:0, y:0}
+	},
+	9110001: {
+		type: "S+U",
+		name: "Schönhauser Allee",
+		geo: {y:52.5493360, x:13.4151380},
+		pos: {x:0, y:0}
+	},
+	9120001: {
+		type: "S+U",
+		name: "Frankfurter Allee",
+		geo: {y:52.5136130, x:13.4753000},
+		pos: {x:0, y:0}
+	},
+	9120004: {
+		type: "S+U",
+		name: "Warschauer Str.",
+		geo: {y:52.5058890, x:13.4487210},
+		pos: {x:0, y:0}
+	},
+	9130002: {
+		type: "S+U",
+		name: "Pankow",
+		geo: {y:52.5672810, x:13.4122790},
+		pos: {x:0, y:0}
+	},
+	9160004: {
+		type: "S+U",
+		name: "Lichtenberg Bhf",
+		geo: {y:52.5107540, x:13.4982280},
+		pos: {x:0, y:0}
+	},
+	9175001: {
+		type: "S+U",
+		name: "Wuhletal",
+		geo: {y:52.5124650, x:13.5754520},
+		pos: {x:0, y:0}
+	}
+};
